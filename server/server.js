40345f939b7aba107582b0cf7e2ef9ac34fb4530
David Fernández <david_ferm@hotmail.com>
2024-01-13 12:33:56 +0100

    cambiar puerto 3049

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const port = process.env.PORT || 3050;
-// const port = 3049;
+// const port = process.env.PORT || 3050;
+const port = 3049;
 
 
 // CORS PARA CONTROLAR LAS PETICIONES QUE RECIBE NUESTRO BACKEND
